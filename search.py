2e6bb404358246656d286e9765e1b77b9f308115
John ShaggyTwoDope Jenkins <john@nixheads.co.uk>
2015-10-31 09:28:33 -0700

    MOAR BIFURCATION

--- search.py
+++ search.py
@@ -3,20 +3,36 @@ import requests
 
 url = "http://ixirc.com/api/?q="
 search = "linux"
-network = "92"
+chanid = "92"
+
+
+class tcolors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    RED = '\033[31m'
+    YELLOW = '\033[33m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 
 def print_search():
-    search = "linux"
-    weathers = requests.get("http://ixirc.com/api/?q=%s" % (search)).json()
+    weathers = requests.get("http://ixirc.com/api/?q=%s&cid=%s" % (search,
+                            chanid)).json()
     jsonData = json.loads(str(weathers).replace("'", '"'))
     jsons = jsonData['results']
     for er in jsons:
         title = er['name']
         packn = er['n']
         botn = er['uname']
-        print(title)
-        print(packn)
-        print(botn)
+        chan = er['cname']
+        netw = er['naddr']
+        size = er['szf']
+        print(tcolors.RED + title + tcolors.GREEN + " from", chan,
+              tcolors.BLUE + "on", netw + tcolors.ENDC)
+        print(tcolors.RED + size + tcolors.YELLOW + " /msg",
+              botn, "xdcc send", packn)
 
 print_search()
